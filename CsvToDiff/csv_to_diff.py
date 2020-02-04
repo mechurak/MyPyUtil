42f88fb4d4116c659f3f86fd801ffdc67158baf6
sshim from desktop <mechurak@gmail.com>
2020-02-04 23:42:09 +0900

    Add hint column

--- CsvToDiff/csv_to_diff.py
+++ CsvToDiff/csv_to_diff.py
@@ -7,13 +7,14 @@ f_out_left = open(OUT_FILE_PRE_FIX+"_left.txt", 'w', encoding='utf-8')
 f_out_right = open(OUT_FILE_PRE_FIX+"_right.txt", 'w', encoding='utf-8')
 
 
-I_INDEX = 0
-I_TITLE = 1
+I_TITLE = 0
+I_INDEX = 1
 I_MEANING = 2
 I_SPELLING = 3
 I_DESCRIPTION = 4
-I_PHONETIC = 5
-I_USAGE = 6
+I_HINT = 5
+I_PHONETIC = 6
+I_USAGE = 7
 
 f = open(INPUT_FILE, 'r', encoding='utf-8')
 csv_reader = csv.reader(f)
@@ -21,12 +22,12 @@ for index, row in enumerate(csv_reader):
     if index == 0:
         continue
     print(index, row[0], row)
-    f_out_left.write(row[I_INDEX] + '. ' + row[I_MEANING] + '\n')
-    f_out_right.write(row[I_INDEX] + '. ' + row[I_MEANING] + '\n')
+    f_out_left.write(row[I_TITLE] + ' - ' + row[I_HINT] + '\n')
+    f_out_right.write(row[I_TITLE] + ' - ' + row[I_HINT] + '\n')
     f_out_left.write(row[I_SPELLING] + '\n')
     f_out_right.write('\n')
-    f_out_left.write(row[I_DESCRIPTION] + '\n\n\n')
-    f_out_right.write(row[I_DESCRIPTION] + '\n\n\n')
+    f_out_left.write('- - -\n\n')
+    f_out_right.write('- - -\n\n')
 
 f.close()
 
