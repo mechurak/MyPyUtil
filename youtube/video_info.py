96cb1bd00b8c519c4b4cd71fbd531867a715c06a
sshim from desktop <mechurak@gmail.com>
2020-09-11 01:09:10 +0900

    Get video info from id

--- youtube/video_info.py
+++ youtube/video_info.py
@@ -4,13 +4,20 @@
 # See instructions for running these code samples locally:
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
-import os
+from typing import List
+import pandas as pd
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+from pandas import DataFrame
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+
+def get_id_list(file_name: str) -> List[str]:
+    df = pd.read_csv(file_name)
+    return df['Id'].to_list()
 
 
 def main():
@@ -29,23 +36,62 @@ def main():
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
-    # id 최대 50개 조회 가능 (50개 넘으면 400 에러)
-    request1 = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
-        id="XM291AfMfO0,ZGFBA9gV5Tg,YgdKbI3nV6U"
-    )
-    response1 = request1.execute()
+    input_df = pd.read_csv('temp.csv')  # DataFrame from the result of video_list.py
+    id_list = input_df['Id'].to_list()
 
-    print(response1)
+    video_id_list = []
+    title_list = []
+    cover_list = []
+    publish_list = []
+    link_list = []
+    view_count_list = []
+    like_count_list = []
+    comment_count_list = []
 
-    # id 최대 50개 조회 가능 (50개 넘으면 400 에러)
-    request2 = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
-        id="Y5HjRn0kvYs,LQNpVpfwvDM,pGNw2yla3kg"
-    )
-    response2 = request2.execute()
+    while id_list:
+        cur_list = id_list[:50]  # id 최대 50개 조회 가능 (50개 넘으면 400 에러)
+        arg_str = ",".join(cur_list)
+        print(len(cur_list), arg_str)
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=arg_str
+        )
+        response = request.execute()
+        items = response["items"]
+        for item in items:
+            video_id = item['id']
+            title = item['snippet']['title'].strip().replace('  ', ' ')
+            cover = f'http://img.youtube.com/vi/{video_id}/sddefault.jpg'
+            publish = item['snippet']['publishedAt'][:10]
+            link = f'https://www.youtube.com/watch?v={video_id}'
+            view_count = int(item['statistics']['viewCount'])
+            like_count = int(item['statistics']['likeCount'])
+            comment_count = int(item['statistics']['commentCount'])
 
-    print(response2)
+            video_id_list.append(video_id)
+            title_list.append(title)
+            cover_list.append(cover)
+            publish_list.append(publish)
+            link_list.append(link)
+            view_count_list.append(view_count)
+            like_count_list.append(like_count)
+            comment_count_list.append(comment_count)
+
+        id_list = id_list[len(cur_list):]
+
+    df = DataFrame({
+        "Id": video_id_list,
+        "Title": title_list,
+        "Cover": cover_list,
+        "Publish": publish_list,
+        "Link": link_list,
+        "View": view_count_list,
+        "Like": like_count_list,
+        "Comment": comment_count_list,
+    })
+    print(df)
+    df.to_csv('output.csv', header=True, index=False)
+    print('to_csv(). Done')
 
 
 if __name__ == "__main__":
